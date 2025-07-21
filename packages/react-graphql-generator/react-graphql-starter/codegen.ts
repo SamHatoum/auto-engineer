e555664ffcc791a1e0fcb60871ee5d4509c32deb
Osama <168728264+osamanar@users.noreply.github.com>
2025-07-21 14:13:03 +0200

    feat(packages/react-graphql-generator): fixes graphql codegen and vite runtime error

--- packages/react-graphql-generator/react-graphql-starter/codegen.ts
+++ packages/react-graphql-generator/react-graphql-starter/codegen.ts
@@ -2,8 +2,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: '../.context/schema.graphql',
-  documents: ['src/gql/**/*.ts', 'src/gql/**/*.tsx'],
+  schema: './schema.graphql',
+  documents: ['src/graphql/**/*.ts', 'src/graphql/**/*.tsx'],
   ignoreNoDocuments: true,
   generates: {
     'src/gql/': {
